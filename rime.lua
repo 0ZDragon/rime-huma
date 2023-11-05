1e928c59f05be4bea3f4f459d85eae9383b7ae6a
0ZDragon <sgm25882@outlook.com>
2023-11-05 19:01:06 +0800

    打词爽的，同时增加单字词组开关

--- rime.lua
+++ rime.lua
@@ -2,3 +2,12 @@
 
 exe_processor = require("exe") -- 网页启动器
 unicode_translator = require("unicode_translator") -- 在Rime輸入任意Unicode字符
+-- rime.lua
+function single_char_filter(input, env)
+  on = env.engine.context:get_option("single_char")
+  for cand in input:iter() do
+    if not on or utf8.len(cand.text) == 1 then
+      yield(cand)
+    end
+  end
+end
